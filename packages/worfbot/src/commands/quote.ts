730e0205d27d771928937c8e44c6c19991b6364d
Pat Jacobs <musicbender@gmail.com>
2026-04-26 17:04:20 -0700

    feedback fix

--- packages/worfbot/src/commands/quote.ts
+++ packages/worfbot/src/commands/quote.ts
@@ -1,7 +1,7 @@
 import type { DiscordInteraction } from '../discord/types';
 import quotes from '../quotes.json';
 
-export async function handleQuote(_interaction: DiscordInteraction): Promise<Response> {
+export async function handleQuote(_: DiscordInteraction): Promise<Response> {
 	const quote = quotes[Math.floor(Math.random() * quotes.length)];
 	return Response.json({
 		type: 4,
