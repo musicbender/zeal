ee3512742ffafaa95e04d320786715a9d6c055cf
Pat Jacobs <musicbender@gmail.com>
2026-05-04 18:18:26 -0700

    more fixes and add github cron

--- apps/gaspar/src/prisma/prisma.service.ts
+++ apps/gaspar/src/prisma/prisma.service.ts
@@ -1,19 +1,18 @@
+import { Pool } from '@neondatabase/serverless';
+import { PrismaNeon } from '@prisma/adapter-neon';
 import { PrismaClient } from '@prisma/client';
 
 export class PrismaService {
 	private prismaClient: PrismaClient;
 
 	constructor() {
-		this.prismaClient = new PrismaClient();
+		const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+		const adapter = new PrismaNeon(pool);
+		this.prismaClient = new PrismaClient({ adapter });
 	}
 
-	// Expose Prisma client methods
 	get sensor() {
 		return this.prismaClient.sensor;
-	}
-
-	async $connect(): Promise<void> {
-		await this.prismaClient.$connect();
 	}
 
 	async $disconnect(): Promise<void> {
